797a6e2871e28dc682058a04dd31397342c317dc
James Burns <jrburns96@aol.com>
2021-11-04 12:55:26 -0400

    Array filtering

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const array = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(arr,search){
+
+    return arr.filter((list) => search.toLowerCase() === list.toLowerCase());
+}
+
+function fuzzyMatch(arr,fuzsearch){
+    return arr.filter((list) => fuzsearch.substr(0,1) === list.substr(0,1));
+}
+
+function matchName(arr, name){
+    return arr.filter((object) => object.name === name);
+
+}
+//fuzzyMatch - This function takes an array of drivers' names and a 
+// string as arguments for querying the array, and returns all drivers 
+// whose names begin with the provided letters.
+
+// function nameis(input){
+//    return str === input;
+//     // return input == str;
+// }
+
+console.log(findMatching(array,"Sa"));
